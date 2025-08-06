d5b97a366ec3fe852d58c1e53ac3478983476c56
Harmin Parra Rueda <harmin.rueda@gmail.com>
2025-08-06 12:24:38 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -27,23 +27,6 @@ BROWSER=${BROWSER,,}
 cp python/file.xml /tmp/
 
 
-echo "#################"
-echo "# Node.js tests #"
-echo "#################"
-echo "node.js: " $(nodejs --version)
-echo "npm: " $(npm --version)
-cd nodejs
-
-echo ==================
-echo Node.js - Cucumber
-echo ==================
-cd cucumber
-npm install > /dev/null
-npx cucumber-js features/catalog.feature
-cd ..
-exit
-
-
 echo "################"
 echo "# Python tests #"
 echo "################"
