c336f88119003daaf44eb06f21c7db1b9d6e491a
Harmin Parra Rueda <harmin.rueda@gmail.com>
2025-08-09 13:57:56 +0200

    Update cypress.config.ts

--- nodejs/cypress/cypress.config.ts
+++ nodejs/cypress/cypress.config.ts
@@ -3,7 +3,7 @@ import { allureCypress } from 'allure-cypress/reporter';
 
 export default defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  video: true,
+  video: false,
   videoCompression: false,
   reporterOptions: {
     charts: true,
