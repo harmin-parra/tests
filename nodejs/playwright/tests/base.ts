773ed5352150275222a29e8a56386de46ad0aca7
Harmin PARRA RUEDA <harmin.rueda@naxos.fr>
2026-01-13 21:47:47 +0100

    update

--- nodejs/playwright/tests/base.ts
+++ nodejs/playwright/tests/base.ts
@@ -1,18 +1,24 @@
 import { test as base } from '@playwright/test';
 import * as allure from "allure-js-commons";
+import { Junit } from '../support/junit-utils';
+import { addJiraReferences, logLastScreenshot } from '../support/utils';
+
+
+const regex = /-\s*C(\d+)$/;
 
 
 export const test = base.extend({
   page: async ({ page }, use, testInfo) => {
+    const match = testInfo.title.match(regex);
+    const caseid: string = match ? match[1] : '0';
+    if (caseid != '0') {
+      await Junit.annotation_case_id(caseid);
+      await allure.tms(caseid);
+      await addJiraReferences(caseid);
+    }
     await use(page);
     if (testInfo.status == 'failed' && !page.isClosed()) {
-      try {
-        await allure.attachment(
-          "Last screenshot before failure",
-          await page.screenshot(),
-          allure.ContentType.PNG
-        );
-      } catch(error) { }
+      await logLastScreenshot(page, caseid);
     }
   },
 });
