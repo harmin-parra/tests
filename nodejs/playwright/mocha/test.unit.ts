dd0b5af9725e0019695fe84e27aa6f6855a55d91
Harmin PARRA RUEDA <harmin.rueda@naxos.fr>
2026-02-22 19:03:22 +0100

    update

--- nodejs/playwright/mocha/test.unit.ts
+++ nodejs/playwright/mocha/test.unit.ts
@@ -1,78 +1,93 @@
 import assert from "node:assert";
-import { typeOf } from "./utils";
+import { typeOf } from "../support/utils";
 
 
 describe("typeOf Tests", function () {
 
   it("typeof Array", function () {
     const type = typeOf([]);
-    assert(type == "Array", "verifying " + type);
+    assert(_type == "Array", "verifying " + _type);
   });
 
 
   it("typeof Date", function () {
-    const type = typeOf(new Date());
-    assert(type == "Date", "verifying " + type);
+    const _type = typeOf(new Date());
+    assert(_type == "Date", "verifying " + _type);
   });
 
 
   it("typeof Null", function () {
-    const type = typeOf(null);
-    assert(type == "Null", "verifying " + type);
+    const _type = typeOf(null);
+    assert(_type == "Null", "verifying " + _type);
   });
 
 
   it("typeof Undefined", function () {
-    const type = typeOf(undefined);
-    assert(type == "Undefined", "verifying " + type);
+    const _type = typeOf(undefined);
+    assert(_type == "Undefined", "verifying " + _type);
   });
 
 
-  it("typeof number primitive", function () {
-    const type = typeOf(123);
-    assert(type == "Number", "verifying " + type);
+  it("typeof String lowercase", function () {
+    const _type = typeOf("hello");
+    assert(_type == "String", "verifying " + _type);
   });
 
 
-  it("typeof Number object", function () {
-    const type = typeOf(Number(123));
-    assert(type == "Number", "verifying " + type);
+  it("typeof Number lowercase", function () {
+    const _type = typeOf(123);
+    assert(_type == "Number", "verifying " + _type);
   });
 
 
-  it("typeof boolean primitive", function () {
-    const type = typeOf(true);
-    assert(type == "Boolean", "verifying " + type);
+  it("typeof Set", function () {
+    const _type = typeOf(new Set());
+    assert(_type == "Set", "verifying " + _type);
+  });
+
+  it("typeof Map", function () {
+    const _type = typeOf(new Map());
+    assert(_type == "Map", "verifying " + _type);
+  });
+
+  it("typeof RegExp", function () {
+    const _type = typeOf(/abc/);
+    assert(_type == "RegExp", "verifying " + _type);
+  });
+
+  it("typeof literal Object", function () {
+    const _type = typeOf({});
+    assert(_type == "Object", "verifying " + _type);
   });
 
 
-  it("typeof Boolean object", function () {
-    const type = typeOf(Boolean(false));
-    assert(type == "Boolean", "verifying " + type);
+  it("typeof Number uppercase", function () {
+    const _type = typeOf(new Number(123));
+    assert(_type == "Number", "verifying " + _type);
   });
 
-
-  it("typeof string primitive", function () {
-    const type = typeOf("hello");
-    assert(type == "String", "verifying " + type);
-  });
-
-
-  it("typeof String object", function () {
-    const type = typeOf(new String("Hello"));
-    assert(type == "String", "verifying " + type);
-  });
-
-
-  it("typeof Object json", function () {
-    const type = typeOf({});
-    assert(type == "Object", "verifying " + type);
+  it("typeof String uppercase", function () {
+    const _type = typeOf(new String(12.78));
+    assert(_type == "String", "verifying " + _type);
   });
 
 
   it("typeof Function", function () {
-    const type = typeOf(() => {});
-    assert(type == "Function", "verifying " + type);
+    const _type = typeOf(() => {});
+    assert(_type == "Function", "verifying " + _type);
+  });
+
+  it("typeof Class", function () {
+    class MyClass {}
+    const _type = typeOf(new MyClass());
+    assert(_type == "MyClass", "verifying " + _type);
+  });
+
+  it("typeof Sub Class", function () {
+    class MySuperClass {}
+    class MySubClass extends MySuperClass{}
+    const _type = typeOf(new MySubClass());
+    assert(_type == "MySubClass", "verifying " + _type);
   });
 
 });
--- nodejs/playwright/mocha/utils.ts
+++
@@ -1,3 +0,0 @@
-export function typeOf(value: any): string {
-  return Object.prototype.toString.call(value).slice(8, -1);
-}
