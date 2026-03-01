553e722af33d142dbf562d798ecc8d4e74ff45a5
Harmin PARRA RUEDA <harmin.rueda@naxos.fr>
2026-03-01 08:44:11 +0100

    update

--- nodejs/playwright/tests/base.ts
+++ nodejs/playwright/tests/base.ts
@@ -23,6 +23,7 @@ export const test = base.extend<Fixtures>({
   page: async ({ page }, use, testInfo) => {
     const match = testInfo.title.match(regex);
     const caseid: string = match ? match[1] : '0';
+    await allure.label("env", testInfo.project.name);
     if (caseid != '0') {
       await Junit.annotation_case_id(caseid);
       await allure.tms(caseid);
