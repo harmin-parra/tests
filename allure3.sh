0c0c6a4413cf3eb5340f0e8c72e1b848b3dede7e
Harmin Parra Rueda <harmin.rueda@gmail.com>
2026-03-22 13:02:50 +0100

    Update allure3.sh

--- allure3.sh
+++ allure3.sh
@@ -3,20 +3,20 @@
 #
 # Java Allure report
 #
-allure awesome \
+npx allure awesome \
   --output reports/allure3-reports/report-java \
   --single-file reports/allure-results/java/playwright
 
 #
 # Node.js Allure report
 #
-allure awesome \
+npx allure awesome \
   --output reports/allure3-reports/report-nodejs \
   --single-file reports/allure-results/nodejs/playwright
 
 #
 # Python Allure report
 #
-allure awesome \
+npx allure awesome \
   --output reports/allure3-reports/report-python \
   --single-file reports/allure-results/python/playwright
